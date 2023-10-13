d8eeef4a2e91aa953735319376d8149b52aaf1d0
anhnhu <anhnt@subwallet.app>
2023-10-13 10:55:58 +0700

    update test webhook

--- scripts/updateDateNow.js
+++ scripts/updateDateNow.js
@@ -1,13 +1,19 @@
-const { ApiPromise, WsProvider } = require('@polkadot/api');
 const fs = require('fs/promises');
-async function main () {
-  const timeElapsed = Date.now();
-const today = new Date(timeElapsed);
-  const data = {
-    dateNow: today.toISOString(),
-  }
 
-  await fs.writeFile('data/today.json', JSON.stringify(data, null, 2));
-  console.log(`You are connected to and save time now`);
+async function main() {
+    const timeElapsed = Date.now();
+    const today = new Date(timeElapsed);
+    let parametrs = '';
+    if (process.argv[2]) {
+        parametrs = process.argv[2];
+    }
+        const data = {
+            dateNow: today.toISOString(),
+            parametrs
+        }
+
+    await fs.writeFile('data/today.json', JSON.stringify(data, null, 2));
+    console.log(`You are connected to and save time now`);
 }
+
 main().catch(console.error).finally(() => process.exit());
